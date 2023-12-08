c1f6167b928b4802559accbce002225396044b7e
MikaelEngvall <mikael@engvall.org>
2023-12-08 17:31:32 +0100

    Fixed counter and the eslint warning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
-import "./App.css"; // Import the CSS file
+import "./App.css";
 
 export default function App() {
   const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [hasMounted, setHasMounted] = useState(false);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
-    setCount((c) => c + 1);
+
+    if (hasMounted) {
+      // Increment count only after the component has mounted
+      setCount((c) => c + 1);
+    }
   }
 
   useEffect(function () {
+    setHasMounted(true);
     getAdvice();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
